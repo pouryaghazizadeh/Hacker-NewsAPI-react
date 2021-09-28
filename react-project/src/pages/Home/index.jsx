26fe9220bad19547f638cb97eeb7ad1833cc8113
pourya ghazizadeh <pourya.gh095@gmail.com>
2021-09-28 23:28:24 +0200

    .

--- react-project/src/pages/Home/index.jsx
+++ react-project/src/pages/Home/index.jsx
@@ -7,8 +7,7 @@ function Home() {
     try {
       // const responce = await axios.get(" https://hacker-news.firebaseio.com/v0/topstories.json")
       const responce = await axios.get(
-        "https://hacker-news.firebaseio.com/v0/topstories.json"
-      );
+        "https://hacker-news.firebaseio.com/v0/topstories.json");
       setData(responce.data);
 
       console.log(data);
