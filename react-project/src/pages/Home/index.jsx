431d4c809bdeea101b68a9d6235a5987583787b2
pourya ghazizadeh <pourya.gh095@gmail.com>
2021-09-28 23:24:33 +0200

    get data

---
+++ react-project/src/pages/Home/index.jsx
@@ -0,0 +1,37 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+function Home() {
+  const [data, setData] = useState([]);
+  const fetchData = async () => {
+    try {
+      // const responce = await axios.get(" https://hacker-news.firebaseio.com/v0/topstories.json")
+      const responce = await axios.get(
+        "https://hacker-news.firebaseio.com/v0/topstories.json"
+      );
+      // console.log(responce);
+      // setData(responce.data)
+      setData(responce.data);
+
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(
+    () => {
+      fetchData();
+    }, // eslint-disable-next-line
+    []
+  );
+
+  const randumNumber = Math.floor(Math.random() * 490) + 1;
+
+ const newRandumData = data.slice(randumNumber, randumNumber + 10);
+ 
+  console.log(newRandumData);
+
+  return <></>;
+}
+
+export default Home;
