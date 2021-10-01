2786c4b6fedd75699b1ea77c90854d3c1b6f1fb0
pourya ghazizadeh <pourya.gh095@gmail.com>
2021-10-01 19:06:12 +0200

    fixing bug

--- react-project/src/components/Home/index.jsx
+++ react-project/src/components/Home/index.jsx
@@ -10,78 +10,67 @@ function Home() {
       const responceData = await axios.get(
         "https://hacker-news.firebaseio.com/v0/topstories.json"
       );
+
       setData(responceData.data);
     } catch (err) {
       console.log(err);
     }
   };
-  useEffect(
-    () => {
-      fetchData();
-    }, // eslint-disable-next-line
-    []
-  );
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   // make randum number for slice array
   const randumNumber = Math.floor(Math.random() * 490) + 1;
-  // slice array for get 10 randumize ids in array 
+  // slice array for get 10 randumize ids in array
   const newRandumData = data.slice(randumNumber, randumNumber + 10);
-
   console.log(newRandumData);
-// make array for get value after map on newRandumData
-const value = []
-const a = []
-// map array of ids for get data
-useEffect(()=>{
-  newRandumData.map((id)=>{
 
-    const getValue = async()=>{
-      const objOfValue = await axios.get(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
-      value.push(objOfValue)
-      console.log(value);
-     return a.push(value)
+  newRandumData.filter((id) => {
+    return getValue(id);
+  });
 
+  const arr = [];
+  
+  async function getValue(id) {
+    try {
+      //  const res =
+      await axios
+        .get(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
+        .then((i) => {
+          return arr.push(i.data);
+        });
+
+      // arr.push(res)
+    } catch (err) {
+      console.log(err);
     }
-    return getValue()
+  }
 
-  })
+  useEffect(
+    () => {
+      getValue();
+    },
+    // eslint-disable-next-line
+    []
+  );
 
-}, // eslint-disable-next-line
-[]
-)
+  console.log(arr);
 
-// console.log(a);
-
-  return <div></div>;
+  return (
+    <>
+    <div>helooo</div>
+      <div>
+        {arr.map((i) => {
+          return (
+            <>
+              <div>{i.score}</div>
+            </>
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
 export default Home;
-
-
-
-
-
-  // newRandumData.map((num) => {
-  //   return fetch(`https://hacker-news.firebaseio.com/v0/item/${num}.json`)
-  //     .then((data) => data.json())
-  //     .then((res) => arrOfobj.push(res));
-  // });
-
-
-
-
-// useEffect(
-//   () => {
-//     newRandumData.map((key)=>{
-//       return async () => {
-//        const er = await axios.get(`https://hacker-news.firebaseio.com/v0/item/${key}.json`)
-//        arrOfobj.push(er.data)
-
-//       console.log(er);
-//       }
-
-//     })
-//   },
-//   // eslint-disable-next-line
-//   [newRandumData]
-//   );
