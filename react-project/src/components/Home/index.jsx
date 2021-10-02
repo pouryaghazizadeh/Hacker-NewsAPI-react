dee14b89c7502dd99fe4c90e259d95857aaff7aa
pourya ghazizadeh <pourya.gh095@gmail.com>
2021-10-02 16:31:19 +0200

    write async function

--- react-project/src/components/Home/index.jsx
+++ react-project/src/components/Home/index.jsx
@@ -2,6 +2,23 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 function Home() {
+  useEffect(()=>{
+    const fetchData = async () => {
+      try{
+
+      }catch(err){
+        console.error(err)
+      }
+    }
+
+    
+
+    // call async function 
+    fetchData()
+
+
+  },// eslint-disable-next-line
+  [])
  
 
   return (
